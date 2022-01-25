25503ff16484e969c984c910ef5774ac31f126a5
alyssalroach <alyroach@umich.edu>
2022-01-25 13:14:32 -0500

    ChartTest3.java

--- jfreechart-1.5.0/target/classes/test/ChartTest3.java
+++ jfreechart-1.5.0/target/classes/test/ChartTest3.java
@@ -1,6 +1,6 @@
-/* ===================
- * Orson Charts - Demo
- * ===================
+/* ==================
+ * BarChartDemo1.java
+ * ==================
  *
  * Copyright 2013-2022, by David Gilbert. All rights reserved.
  *
@@ -28,94 +28,105 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * 
- * Note that the above terms apply to the demo source only, and not the 
- * Orson Charts library.
- * 
  */
 
-package com.orsoncharts.demo;
+package test;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
-import org.jfree.chart3d.Chart3D;
-import org.jfree.chart3d.Chart3DFactory;
-import org.jfree.chart3d.Chart3DPanel;
-import org.jfree.chart3d.data.category.CategoryDataset3D;
-import org.jfree.chart3d.graphics3d.swing.DisplayPanel3D;
-import org.jfree.chart3d.plot.CategoryPlot3D;
-import org.jfree.chart3d.renderer.category.AreaRenderer3D;
+import java.awt.Font;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.RadialGradientPaint;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.chart.ui.ApplicationFrame;
+import org.jfree.chart.ui.UIUtils;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.awt.*;
 
 /**
- * A demo of a 3D area chart in Swing.
+ * A simple demonstration application showing how to create a bar chart.
  */
-public class AreaChart3DDemo1 extends JFrame {
+public class ChartTest3 {
 
-    /**
-     * Creates a new test app.
-     *
-     * @param title  the frame title.
-     */
-    public AreaChart3DDemo1(String title) {
-        super(title);
-        addWindowListener(new ExitOnClose());
-        getContentPane().add(createDemoPanel());
-    }
+    private static JFreeChart createChart(CategoryDataset dataset) {
 
-    /**
-     * Returns a panel containing the content for the demo.  This method is
-     * used across all the individual demo applications to allow aggregation 
-     * into a single "umbrella" demo (OrsonChartsDemo).
-     * 
-     * @return A panel containing the content for the demo.
-     */
-    public static DemoPanel createDemoPanel() {
-        DemoPanel content = new DemoPanel(new BorderLayout());
-        content.setPreferredSize(OrsonChartsDemo.DEFAULT_CONTENT_SIZE);
-        CategoryDataset3D<String, String, String> dataset = SampleData.createCompanyRevenueDataset();
-        Chart3D chart = createChart(dataset);
-        Chart3DPanel chartPanel = new Chart3DPanel(chart);
-        content.setChartPanel(chartPanel);
-        content.add(new DisplayPanel3D(chartPanel));
-        chartPanel.zoomToFit(OrsonChartsDemo.DEFAULT_CONTENT_SIZE);
-        return content;
-    }
-    
-    /**
-     * Creates an area chart using the specified {@code dataset}.
-     * 
-     * @param dataset  the dataset.
-     * 
-     * @return An area chart. 
-     */
-    private static Chart3D createChart(CategoryDataset3D<String, String, String> dataset) {
-        Chart3D chart = Chart3DFactory.createAreaChart(
-                "Reported Revenues By Quarter", 
-                "Large companies in the IT industry", dataset, "Company", 
-                "Quarter", "Value");
-        chart.setChartBoxColor(new Color(255, 255, 255, 128));
-        CategoryPlot3D plot = (CategoryPlot3D) chart.getPlot();
-        plot.getRowAxis().setVisible(false);
-        plot.setGridlineStrokeForValues(new BasicStroke(0.5f, 
-                BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 4.0f, 
-                new float[] { 2f, 2f }, 0f));
-        AreaRenderer3D renderer = (AreaRenderer3D) plot.getRenderer();
-        renderer.setBaseColor(Color.GRAY);
+        JFreeChart chart = ChartFactory.createBarChart(
+            "Performance: JFreeSVG vs Batik", null /* x-axis label*/, 
+                "Milliseconds" /* y-axis label */, dataset);
+        chart.addSubtitle(new TextTitle("Time to generate 1000 charts in SVG " 
+                + "format (lower bars = better performance)"));
+        chart.setBackgroundPaint(Color.WHITE);
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
         return chart;
+
     }
 
+    private static CategoryDataset createDataset() {
+
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(7445, "JFreeSVG", "Warm-up");
+        dataset.addValue(24448, "Batik", "Warm-up");
+        dataset.addValue(4297, "JFreeSVG", "Test");
+        dataset.addValue(21022, "Batik", "Test");
+
+        return dataset;
+    }
 
     /**
-     * Starting point for the app.
+     * Starting point for the demonstration application.
      *
-     * @param args  command line arguments (ignored).
+     * @param args  ignored.
+     * @throws IOException
      */
     public static void main(String[] args) {
-        AreaChart3DDemo1 app = new AreaChart3DDemo1(
-                "OrsonCharts: AreaChart3DDemo1.java");
-        app.pack();
-        app.setVisible(true);
+        CategoryDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+
+        BufferedImage img = new BufferedImage(1, 1,
+        BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = img.createGraphics();
+        g2.dispose();
+
+        // now create an image of the required size for the legend
+        int w = (int) Math.rint(800);
+        int h = (int) Math.rint(600);
+        BufferedImage img2 = new BufferedImage(w, h,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g22 = img2.createGraphics();
+        chart.draw(g22, new Rectangle2D.Double(0, 0, w, h));
+        g22.dispose();
+
+        // ...and save it to a PNG image
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(
+                new File("output2.png")));
+        ChartUtils.writeBufferedImageAsPNG(out, img2);
+        out.close();
+        System.out.println("output2.png created"); 
     }
 
 }
